b0e3f5e5d70fe43fe9cfeb3dd9c50f279ed0d2aa
Jin Li <69916725+lijh724@users.noreply.github.com>
2020-08-19 13:42:23 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,18 @@
+/****************
+* Hello.java
+*****************
+* Jin Li
+* August 19, 2020
+* CSMC255-002
+*****************/
+
+public class Hello {
+	public static void main(String[] args) {
+
+		System.out.println("Jin Li");
+		System.out.println("Hello.java");
+		System.out.println("Lab 1: creating a program in java");
+		System.out.println("CSMC255-002");
+		System.out.println();
+	}
+}
